c66edf6d6de70d279ab8ac3d3311662e86ca56c8
Le Thanh Tung <letung767768@gmail.com>
2024-10-19 21:16:09 +0700

    Task 19-10: next preview image

--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -7,6 +7,7 @@ import { getAllCodeService } from '../../services/userService';
 export const fetchGenderStart = () => {
      return async (dispatch, getState) => {
           try {
+               dispatch({ type: actionTypes.FETCH_GENDER_START })
                let res = await getAllCodeService('gender');
                if (res && res.errCode === 0) {
                     dispatch(fetchGenderSuccess(res.data))
@@ -33,6 +34,72 @@ export const fetchGenderSuccess = (genderData) => ({
 export const fetchGenderFailed = () => ({
      type: actionTypes.FETCH_GENDER_FAILDED
 })
+
+
+export const fetchPositionSuccess = (positionData) => ({
+     type: actionTypes.FETCH_POSITION_SUCCESS,
+     data: positionData
+
+})
+
+
+export const fetchPositionFailed = () => ({
+     type: actionTypes.FETCH_POSITION_FAILDED
+})
+
+
+export const fetchRoleSuccess = (roleData) => ({
+     type: actionTypes.FETCH_ROLE_SUCCESS,
+     data: roleData
+
+})
+
+
+export const fetchRoleFailed = () => ({
+     type: actionTypes.FETCH_ROLE_FAILDED
+})
+
+
+export const fetchPositionStart = () => {
+     return async (dispatch, getState) => {
+          try {
+               dispatch({ type: actionTypes.FETCH_GENDER_START })
+               let res = await getAllCodeService('position');
+               if (res && res.errCode === 0) {
+                    dispatch(fetchPositionSuccess(res.data))
+               }
+               else {
+                    dispatch(fetchPositionFailed())
+               }
+
+          } catch (error) {
+               dispatch(fetchPositionFailed())
+               console.log("fetch position Start error: ", error)
+          }
+     }
+
+}
+
+export const fetchRoleStart = () => {
+     return async (dispatch, getState) => {
+          try {
+               dispatch({ type: actionTypes.FETCH_GENDER_START })
+               let res = await getAllCodeService('role');
+               if (res && res.errCode === 0) {
+                    dispatch(fetchRoleSuccess(res.data))
+               }
+               else {
+                    dispatch(fetchRoleFailed())
+               }
+
+          } catch (error) {
+               dispatch(fetchRoleFailed())
+               console.log("fetch position Start error: ", error)
+          }
+     }
+
+}
+
 
 
 
